9519a9f310a4c90363489abaa309b4f24e6f9cfe
NelloKudo <marshnelloosu@gmail.com>
2025-11-01 17:51:17 +0100

    bump: winello-cachyos-v10.0-2
    Patchset/Changelogs: https://github.com/whrvt/wine-osu-patches/tree/winello-v10.15-2

--- wine_builder.sh
+++ wine_builder.sh
@@ -71,9 +71,8 @@ _configuration() {
     # osu!-specific settings
     if [ "$WINE_OSU" == "true" ]; then
         BUILD_NAME="wine-osu-cachy"
-        USE_STAGING="true"
-        RELEASE_VERSION="1"
-        PATCHSET="remote:winello-cachy-v10.0-$RELEASE_VERSION"
+        RELEASE_VERSION="2"
+        PATCHSET="remote:winello-cachyos-v10.0-$RELEASE_VERSION"
         USE_TKG="false"
     fi
 
@@ -344,6 +343,7 @@ build_setup() {
         --without-v4l2
         --without-netapi
         --disable-msv1_0
+        --disable-lsteamclient
     )
 
     if [ "${DEBUG}" = "true" ]; then
